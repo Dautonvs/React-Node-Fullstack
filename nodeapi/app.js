43b22ce91603feac73418b43e1e65b0770b776e0
Dnvs <dauto.nvs@gmail.com>
2019-05-10 23:06:24 -0300

    Node API - First Steps

---
+++ nodeapi/app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const dotenv = require("dotenv");
+dotenv.config();
+
+//db connection
+mongoose
+  .connect(process.env.MONGO_URI, {useNewUrlParser: true})
+.then(() => console.log("DB Connected!"));
+   mongoose.connection.on('error', err => {
+  console.log(`DB connection error: ${err.message}`);
+});
+
+const postRoutes = require('./routes/post');
+
+// middleware
+app.use(morgan("dev"));
+app.use( "/", postRoutes);
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+    console.log(`This API is listening on port: ${port}`);
+    });
